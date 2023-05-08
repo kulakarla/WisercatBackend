e178b85e718accfcdb4090fedc72edd51866fc45
kulakarla <sderrtsee@gmail.com>
2023-05-08 21:39:32 +0300

    README, added more initial pets

--- src/main/resources/db/changelog/changes/6-pets-pop.sql
+++ src/main/resources/db/changelog/changes/6-pets-pop.sql
@@ -1,3 +1,9 @@
-INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('12312312', 'Herbert', 'Dog', 'Red', 'Estonia', 1);
-INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('34343434', 'Lerman', 'Ferret', 'Brown', 'Finland', 3);
-INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('56565656', 'Toomas', 'Cat', 'White', 'Sweden', 1);
+INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('37826379', 'Herbert', 'Dog', 'Red', 'Estonia', 1);
+INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('22242402', 'Lerman', 'Ferret', 'Brown', 'Finland', 3);
+INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('99743072', 'Toomas', 'Cat', 'White', 'Sweden', 1);
+INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('71830542', 'Anxie', 'Hamster', 'Green', 'Sudan', 2);
+INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('67955718', 'Batman', 'Rabbit', 'Orange', 'Solomon Islands', 2);
+INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('81203806', 'Marta', 'Parrot', 'Black', 'Tunisia', 3);
+INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('53738595', 'Yoelle', 'Horse', 'Blue', 'Sweden', 3);
+INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('23850953', 'Bart', 'Cat', 'Yellow', 'Paraguay', 2);
+INSERT INTO pets(id_code, name, animal, color, country, user_id) VALUES ('63795112', 'JP Morgan', 'Dog', 'White', 'United States', 1);
